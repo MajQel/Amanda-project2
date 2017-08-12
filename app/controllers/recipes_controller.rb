582bf2c9342a0b56be26264bfcebaeb95be20e2e
MajQel <Despirage@gmail.com>
2017-08-12 19:53:36 +0200

    form added

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,29 @@ class RecipesController < ApplicationController
 	end
 
 	def show
+
 	end
 
-	
+	def new
+		@recipe = Recipe.new
+	end
+
+	def create
+		@recipe = Recipe.new[recipe_params]
+	end
+
+	private
+
+	def recipe_params
+		params.requie[:recipe].permit[:title, :desciption]
+	end
+
+	def find_recipe
+		@recipe = Recipe.find[params[:id]]
+	end
+
+
+
+
+
 end
