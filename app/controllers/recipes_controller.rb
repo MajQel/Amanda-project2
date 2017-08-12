657340ada91904014f73a82f6151d80c0cb40c40
MajQel <Despirage@gmail.com>
2017-08-12 20:41:02 +0200

    Full scaffold presented (new missing)

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,7 @@ class RecipesController < ApplicationController
 	before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 
 	def index
+		@recipe = Recipe.all.order("created_at DESC")
 	end
 
 	def show
@@ -13,7 +14,7 @@ class RecipesController < ApplicationController
 	end
 
 	def create
-		@recipe = Recipe.new[recipe_params]
+		@recipe = Recipe.new(recipe_params)
 
 		if @recipe.save
 			redirect_to @recipe, notice: "Successfuly created new recipe"
@@ -22,14 +23,31 @@ class RecipesController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+	def update
+		if @recipe.update(recipe_params)
+			redirect_to @recipe
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@recipe.destroy
+		redirect_to root_path, notice: "Przepis zostal usuniety"
+	end
+
+
 	private
 
 	def recipe_params
-		params.requie[:recipe].permit[:title, :desciption]
+		params.require(:recipe).permit(:tittle, :description)
 	end
 
 	def find_recipe
-		@recipe = Recipe.find[params[:id]]
+		@recipe = Recipe.find(params[:id])
 	end
 
 
