78df8f1c39feb15beb6c3f2585068ba425bb7a3f
MajQel <Despirage@gmail.com>
2017-08-16 20:15:21 +0200

    Added time neccessery to prepare meal

--- db/migrate/20170816181156_add_preparation_time_to_recipe.rb
+++ db/migrate/20170816181156_add_preparation_time_to_recipe.rb
@@ -1,5 +1,5 @@
 class AddPreparationTimeToRecipe < ActiveRecord::Migration[5.0]
   def change
-    add_column :recipes, :przeparation_time, :number
+    add_column :recipes, :preparation_time, :number
   end
 end
