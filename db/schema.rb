271e73df9e191abab67875a4e16d8209d34b9f2c
MajQel <Despirage@gmail.com>
2017-08-16 20:52:52 +0200

    Portions + time needed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816181156) do
+ActiveRecord::Schema.define(version: 20170816183736) do
 
   create_table "directions", force: :cascade do |t|
     t.text     "step"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20170816181156) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.decimal  "preparation_time"
+    t.integer  "portion"
   end
 
   create_table "skladniks", force: :cascade do |t|
