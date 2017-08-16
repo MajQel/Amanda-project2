c27db352e6dd1adeed49f852a2bd962449392e02
MajQel <Despirage@gmail.com>
2017-08-16 20:01:40 +0200

    New column has been added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170813111957) do
+ActiveRecord::Schema.define(version: 20170816180106) do
 
   create_table "directions", force: :cascade do |t|
     t.text     "step"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20170813111957) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.decimal  "przeparation_time"
   end
 
   create_table "skladniks", force: :cascade do |t|
